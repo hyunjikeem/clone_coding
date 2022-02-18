5b6f732ee3b51d932d5da39fb52f17b64c649d52
HyunjiKim818 <kjhxxxx@gmail.com>
2022-02-18 20:41:06 +0900

    큰 틀 완성

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-setting
+const express = require('express');
+const app = express();
+const port = 5000;
+
+const connect = require('./schemas');
+connect();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const user = require('./routes/user');
+app.use('/api', user);
+
+app.listen(port, () => {
+    console.log(`listening at http://localhost:${port}`);
+});
