9813d9802c174de001d2438ff28dd8ba34878970
Tacocat3 <yongjusung3351@gmail.com>
2022-02-23 22:19:36 +0900

    1

--- schemas/index.js
+++ schemas/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connect = () => {
     mongoose.connect('mongodb://test:test@localhost:27017/admin', {
-        dbName: 'airb2b',
+        dbName: 'airb3b',
         // useNewUrlParser: true,
         // useUnifiedTopology: true,
         ignoreUndefined: true
